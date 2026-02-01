af12af3b9853ef18c2767112ed015c093b2bb95a
oboktk <obidikesamuel@gmail.com>
2026-02-01 18:48:18 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dixgcjd5d/video/upload/Patrick_Hizon_EJEAN_-_Sweatshirt_feat._Gabe_Bondoc_Lyric_Video_-_Patrick_Hizon_youtube_hjnhq9.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
